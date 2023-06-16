87d5025544bba49204e7740ec55b2d7ab9172e82
Kevin Tersigni <ktersigni@sqli.com>
2023-06-16 06:17:21 +0200

    update config files for imports and alias with new folders

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,95 +1,93 @@
 module.exports = {
-    env: {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'google',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    '@react-native-community',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        "plugin:react/recommended",
-        "google",
-        "eslint:recommended",
-        "plugin:import/errors",
-        "plugin:import/warnings",
-        "plugin:import/typescript",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
-        "@react-native-community",
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    plugins: [
-        "react",
-        "@typescript-eslint"
-    ],
-    rules: {
-        "indent": ["error", 2, { "SwitchCase": 1 }],
-        "react-native/no-inline-styles": "off",
-        "no-duplicate-imports": ["error"],
-        "@typescript-eslint/explicit-module-boundary-types": 0,
-        "@typescript-eslint/no-var-requires": 0,
-        "@typescript-eslint/ban-ts-comment": "off",
-        "no-shadow": 0,
-        "react-native/no-unused-styles": 2,
-        "react-native/no-color-literals": 2,
-        "react-native/no-single-element-style-arrays": 2,
-        "import/order": [
-          "error",
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'react-native/no-inline-styles': 'off',
+    'no-duplicate-imports': ['error'],
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-shadow': 0,
+    'react-native/no-unused-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['sibling', 'parent']],
+        pathGroups: [
           {
-            "groups": ["builtin", "external", "internal", ["sibling", "parent"]],
-            "pathGroups": [
-              {
-                "pattern": "react",
-                "group": "external",
-                "position": "before",
-              },
-              {
-                "pattern": "react-native",
-                "group": "external",
-                "position": "before",
-              },
-            ],
-            "pathGroupsExcludedImportTypes": ["builtin"],
-            "newlines-between": "always",
-            "alphabetize": {
-              "order": "asc",
-              "caseInsensitive": true,
-            },
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react-native',
+            group: 'external',
+            position: 'before',
           },
         ],
-    },
-    settings: {
-      "import/resolver": {
-        node: {
-          extensions: [
-            ".js",
-            ".jsx"
-          ]
-        },
-        typescript: true,
-        alias: {
-          map: [
-            ['@assets', './src/assets'],
-            ['@components', './src/components'],
-            ['@constants', './src/constants'],
-            ['@enums', './src/enums'],
-            ['@hooks', './src/hooks'],
-            ['@models', './src/models'],
-            ['@navigators', './src/navigators'],
-            ['@redux', './src/redux'],
-            ['@screens', './src/screens'],
-            ['@services', './src/services'],
-            ['@theme', './src/theme'],
-            ['@utils', './src/utils'],
-          ],
-          extensions: ['.ts', '.tsx', '.ios.ts', '.android.ts', '.json'],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
         },
       },
-      'import/ignore': ['react-native'],
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+      typescript: true,
+      alias: {
+        map: [
+          ['@assets', './src/assets'],
+          ['@components', './src/components'],
+          ['@constants', './src/constants'],
+          ['@enums', './src/enums'],
+          ['@hooks', './src/hooks'],
+          ['@models', './src/models'],
+          ['@navigators', './src/navigators'],
+          ['@redux', './src/redux'],
+          ['@screens', './src/screens'],
+          ['@services', './src/services'],
+          ['@theme', './src/theme'],
+          ['@utils', './src/utils'],
+          ['@thunks', './src/redux/thunks'],
+          ['@selectors', './src/redux/selectors'],
+          ['@middlewares', './src/redux/middlewares'],
+          ['@reducers', './src/redux/reducers'],
+        ],
+        extensions: ['.ts', '.tsx', '.ios.ts', '.android.ts', '.json', '.js'],
+      },
     },
-}
+    'import/ignore': ['react-native'],
+  },
+};
