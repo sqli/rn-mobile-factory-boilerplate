647fdfc282b155c7330ea578c1da7157f8f3a6b6
jljonnard <jljonnard@sqli.com>
2024-08-14 15:47:24 +0200

    feat: add keychain manager and authent slice delete user middleware as it is not used in other project

---
+++ template/src/managers/KeychainManager.ts
@@ -0,0 +1,42 @@
+import * as Keychain from 'react-native-keychain';
+
+const KeychainManager = {
+  getItem: (key: string) =>
+    new Promise((res, rej) => {
+      Keychain.getInternetCredentials(key)
+        .then(result => {
+          if (result === false || result?.password === 'null') {
+            res(null);
+          } else {
+            res(result?.password);
+          }
+        })
+        .catch(e => {
+          rej(e);
+        });
+    }),
+
+  setItem: (key: string, item: string, username: string) =>
+    new Promise<void>((res, rej) => {
+      Keychain.setInternetCredentials(key, username ?? 'null', item ?? 'null')
+        .then(() => {
+          res();
+        })
+        .catch(e => {
+          rej(e);
+        });
+    }),
+
+  removeItem: (key: string) =>
+    new Promise<void>((res, rej) => {
+      Keychain.resetInternetCredentials(key)
+        .then(() => {
+          res();
+        })
+        .catch(e => {
+          rej(e);
+        });
+    }),
+};
+
+export default KeychainManager;
