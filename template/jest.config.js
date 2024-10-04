f85378aa2883cd267d6714a3d7c73dc74e25b0c7
jljonnard <jljonnard@sqli.com>
2024-10-04 15:49:05 +0200

    ci: jest ocnfig to report on sonar

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ['./jest.setup.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testResultsProcessor: 'jest-sonar-reporter',
 };
