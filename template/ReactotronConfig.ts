19184030111b18b8a8f5ff6b4a033e29d1d34ff3
Floris Mettey <fmettey@sqli.com>
2024-12-10 18:59:25 +0100

    fix: iOS packager not connected because of new architecture + Reactotron hostname

--- template/ReactotronConfig.ts
+++ template/ReactotronConfig.ts
@@ -1,5 +1,3 @@
-import { NativeModules } from 'react-native';
-
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
@@ -19,13 +17,14 @@ let reactotron;
 
 let scriptHostname;
 if (__DEV__) {
-  const scriptURL = NativeModules.SourceCode.scriptURL;
-  scriptHostname = scriptURL.split('://')[1].split(':')[0];
+  // TODO: find a solution for Reactotron hostname + new architecture
+  // const scriptURL = NativeModules.SourceCode.scriptURL;
+  // scriptHostname = scriptURL.split('://')[1].split(':')[0];
 
   reactotron =
     Reactotron.setAsyncStorageHandler &&
     Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-      .configure({ name: 'boilerplate', host: scriptHostname }) // controls connection & communication settings
+      .configure({ name: 'boilerplate' /*, host: scriptHostname */ }) // controls connection & communication settings
       .useReactNative() // add all built-in react native plugins
       .use(reactotronRedux())
       .connect(); // let's connect!
